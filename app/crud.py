14754c3b3e86879886c2a7d3ad671caeebe7a265
Rvoke <rvoke@gmail.com>
2025-09-06 13:55:29 +0300

    Created UserCRUD

---
+++ app/crud.py
@@ -0,0 +1,63 @@
+from datetime import date
+
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.models import User
+from database import async_session_factory
+
+class UserCRUD:
+  def __init__(self):
+    pass
+
+  async def create_user(
+    self,
+    session: AsyncSession, 
+    tg_id: int,
+    username: str
+  ):
+    user = User(
+      tg_id=tg_id,
+      username=username
+    )
+    session.add(user)
+    
+  async def get_user(
+    self,
+    session: AsyncSession,
+    tg_id: int,
+  ) -> User | None:
+    query = (
+      select(User)
+      .where(User.tg_id == tg_id)
+    )
+
+    user = (await session.execute(query)).scalar_one_or_none()
+
+    return user
+
+  async def change_user_data(
+    self,
+    session: AsyncSession,
+    *,
+    tg_id: int = None,
+    username: str = None,
+    name: str = None,
+    birthday: date = None,
+    photo_path: str = None
+  ):
+    user = await self.get_user(session, tg_id)
+
+    if not user:
+      return
+    
+    if username:
+      user.username = username
+    if name:
+      user.name = name
+    if birthday:
+      user.birthday = birthday
+    if photo_path:
+      user.photo_path = photo_path
+
+user_crud = UserCRUD()
