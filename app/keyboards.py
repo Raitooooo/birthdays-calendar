ba0cad375b4e0693109aff6ada9727d2c7bdadca
Rvoke <rvoke@gmail.com>
2025-09-06 14:40:06 +0300

    Changed keyboard

--- app/keyboards.py
+++ app/keyboards.py
@@ -5,4 +5,5 @@ from aiogram.types import (
 
 start_keyboard = ReplyKeyboardMarkup(keyboard=[
   [KeyboardButton(text='Посмотреть Календарь'), KeyboardButton(text='Профиль')]
+  
 ], resize_keyboard=True) 
